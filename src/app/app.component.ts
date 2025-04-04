3a6a1601e18c03edf08decb48a1944629d3cfe8a
chris-ian-jones <chris.ian.jones@outlook.com>
2025-04-04 16:30:33 +0100

    remove redundant routes usage, and updated build options to suppress dependancy warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SwapComponent } from './components/swap/swap.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SwapComponent],
+  imports: [SwapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
